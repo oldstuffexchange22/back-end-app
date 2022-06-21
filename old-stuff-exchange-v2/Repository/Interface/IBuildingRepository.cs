b3288c9a7a98676eaf79deed959a4096b053771e
tannvv <nvtan.a5@gmail.com>
2022-06-21 12:55:41 +0700

    update search bulding

--- old-stuff-exchange-v2/Repository/Interface/IBuildingRepository.cs
+++ old-stuff-exchange-v2/Repository/Interface/IBuildingRepository.cs
@@ -10,7 +10,7 @@ namespace Old_stuff_exchange.Repository.Interface
         Task<Building> Create(Building building);
         Task<Building> Update(Building building);
         Task<bool> Delete(Guid ID);
-        List<Building> GetList(Guid? apartmentId ,int page, int pageSize);
+        List<Building> GetList(Guid? apartmentId ,string name, int page, int pageSize);
         Task<Building> GetById(Guid ID);
     }
 }
