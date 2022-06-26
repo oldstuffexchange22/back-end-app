3d6457be2ee4ad7a16c595e7b36379a74d9f42aa
tannvv <nvtan.a5@gmail.com>
2022-06-27 01:32:54 +0700

    notification service

---
+++ old-stuff-exchange-v2/Controllers/NotificationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Old_stuff_exchange.Controllers;
+using old_stuff_exchange_v2.Model;
+using old_stuff_exchange_v2.Service;
+using System.Threading.Tasks;
+
+namespace old_stuff_exchange_v2.Controllers
+{
+    public class NotificationController : BaseApiController
+    {
+        private readonly INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendNotification(NotificationModel notificationModel)
+        {
+            return Ok(await _notificationService.SendNotification(notificationModel));
+        }
+    }
+}
---
+++ old-stuff-exchange-v2/Service/NotificationService.cs
@@ -0,0 +1,68 @@
+﻿using CorePush.Google;
+using Microsoft.Extensions.Options;
+using old_stuff_exchange_v2.Configs;
+using old_stuff_exchange_v2.Model;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace old_stuff_exchange_v2.Service
+{
+    public interface INotificationService
+    {
+        Task<string> SendNotification(NotificationModel notificationModel);
+    }
+    public class NotificationService : INotificationService
+    {
+        private readonly FcmNotificationSetting _fcmNotificationSetting;
+        public NotificationService(IOptions<FcmNotificationSetting> settings)
+        {
+            _fcmNotificationSetting = settings.Value;
+        }
+        public async Task<string> SendNotification(NotificationModel notificationModel)
+        {
+            string response = "";
+            try
+            {
+                FcmSettings settings = new FcmSettings()
+                {
+                    ServerKey = _fcmNotificationSetting.ServerKey,
+                    SenderId = _fcmNotificationSetting.SenderId
+                };
+                HttpClient httpClient = new HttpClient();
+                string authorizationKey = string.Format("key={0}", settings.ServerKey);
+                string deviceToken = notificationModel.DeviceId;
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", authorizationKey);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                GoogleNotification.DataPayload dataPayload = new GoogleNotification.DataPayload();
+                dataPayload.Title = notificationModel.Title;
+                dataPayload.Body = notificationModel.Body;
+
+                GoogleNotification notification = new GoogleNotification();
+                notification.Data = dataPayload;
+                notification.Notification = dataPayload;
+
+                var fcm = new FcmSender(settings, httpClient);
+                var fcmSendResponse = await fcm.SendAsync(deviceToken, notification);
+                if (fcmSendResponse.IsSuccess())
+                {
+                    response = "Notification sent successfully";
+                    return response;
+                }
+                else
+                {
+                    response = fcmSendResponse.Results[0].Error;
+                    return response;
+                }
+
+            }
+            catch (Exception)
+            {
+                response = "Something went wrong";
+                return response;
+            }
+        }
+    }
+}
