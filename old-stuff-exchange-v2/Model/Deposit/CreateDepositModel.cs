f703500f9fc5ff5f7be3c314313cef9e3b4b7410
tannvv <nvtan.a5@gmail.com>
2022-07-16 14:05:30 +0700

    update api transaction

--- old-stuff-exchange-v2/Model/Deposit/CreateDepositModel.cs
+++ old-stuff-exchange-v2/Model/Deposit/CreateDepositModel.cs
@@ -5,7 +5,7 @@ namespace old_stuff_exchange_v2.Model.Deposit
     public class CreateDepositModel
     {
         public string WalletElectricName { get; set; }
-        public string Descripion { get; set; }
+        public string Description { get; set; }
         public decimal Amount { get; set; }
         public Guid UserId { get; set; }
     }
