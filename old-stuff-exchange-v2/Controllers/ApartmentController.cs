c17cc72d12b1cc2233ac9721c92bfbf270941175
tannvv <nvtan.a5@gmail.com>
2022-07-18 10:28:25 +0700

    dont authorize apartment id

--- old-stuff-exchange-v2/Controllers/ApartmentController.cs
+++ old-stuff-exchange-v2/Controllers/ApartmentController.cs
@@ -30,6 +30,7 @@ namespace old_stuff_exchange_v2.Controllers
 
         [HttpGet("{id}")]
         [SwaggerOperation(Summary = "Get apartment by id")]
+        [AllowAnonymous]
         [Cache(1)]
         public async Task<IActionResult> GetById(Guid id)
         {
